20c449595dba60b071bf8ac61b48b4c628f1f979
shbhm0 <shubham.mishra@appinventiv.com>
2021-08-11 13:07:12 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [],
+    };
+  }
+  async getMovies() {
+    try {
+      const response = await fetch(
+        'https://mobilecdn.6thstreet.com/resources/20191010_staging/en-ae/men.json',
+      );
+      const json = await response.json();
+      this.setState({data: json});
+    } catch (error) {
+      console.log(error);
+    } finally {
+      this.setState({isLoading: false});
+    }
+  }
+  componentDidMount() {
+    this.getMovies();
+  }
+  render() {
+    console.log(this.state.data);
+    return (
+      <View>
+        <Text> App </Text>
+      </View>
+    );
+  }
+}
