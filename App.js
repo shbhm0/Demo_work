b3d1d7a3f8e976c32f31ee2675323f55a6faf992
Alok-Gahlot <alok.gahlo@appinventiv.com>
2021-08-11 16:39:53 +0530

    alok

--- App.js
+++ App.js
@@ -30,6 +30,7 @@ export default class App extends React.Component {
         <Text> App </Text>
         <View>
           <Text>Hello</Text>
+          <Text>Hello Alok</Text>
         </View>
       </View>
     );
